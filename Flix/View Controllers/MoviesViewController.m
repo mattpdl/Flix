b3d925674c5a7b231cf2cd927dd9bb131d060781
Matthew John Ponce de Leon <mattpdl@fb.com>
2021-06-24 18:02:23 -0700

    Setup request to The Movie Database API

---
+++ Flix/View Controllers/MoviesViewController.m
@@ -0,0 +1,54 @@
+//
+//  MoviesViewController.m
+//  Flix
+//
+//  Created by mattpdl on 6/24/21.
+//
+
+#import "MoviesViewController.h"
+
+@interface MoviesViewController ()
+
+@property (nonatomic, strong) NSArray *movies;
+
+@end
+
+@implementation MoviesViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    
+    NSURL *url = [NSURL URLWithString:@"https://api.themoviedb.org/3/movie/now_playing?api_key=82066c445c715af844c04703a9f38b33"];
+    NSURLRequest *request = [NSURLRequest requestWithURL:url cachePolicy:NSURLRequestReloadIgnoringLocalCacheData timeoutInterval:10.0];
+    NSURLSession *session = [NSURLSession sessionWithConfiguration:[NSURLSessionConfiguration defaultSessionConfiguration] delegate:nil delegateQueue:[NSOperationQueue mainQueue]];
+    NSURLSessionDataTask *task = [session dataTaskWithRequest:request completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+           if (error != nil) {
+               NSLog(@"%@", [error localizedDescription]);
+           }
+           else {
+               NSDictionary *dataDictionary = [NSJSONSerialization JSONObjectWithData:data options:NSJSONReadingMutableContainers error:nil];
+               //NSLog(@"%@", dataDictionary);
+
+               // Get the array of movies and store the movies in a property to use elsewhere
+               self.movies = dataDictionary[@"results"];
+               for (NSDictionary *movie in self.movies)
+                   NSLog(@"%@", movie[@"original_title"]);
+               
+               // TODO: Reload your table view data
+           }
+       }];
+    [task resume];
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
