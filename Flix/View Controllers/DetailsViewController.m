a2777a82014f515e651b0b393abd5118a6a03c75
Matthew John Ponce de Leon <mattpdl@fb.com>
2021-06-25 10:31:55 -0700

    Add placeholder details view for movies

---
+++ Flix/View Controllers/DetailsViewController.m
@@ -0,0 +1,37 @@
+//
+//  DetailsViewController.m
+//  Flix
+//
+//  Created by mattpdl on 6/25/21.
+//
+
+#import "DetailsViewController.h"
+#import "UIImageView+AFNetworking.h"
+
+@interface DetailsViewController ()
+
+@property (weak, nonatomic) IBOutlet UIImageView *backdropView;
+@property (weak, nonatomic) IBOutlet UIImageView *posterView;
+@property (weak, nonatomic) IBOutlet UILabel *titleLabel;
+@property (weak, nonatomic) IBOutlet UILabel *synopsisLabel;
+
+@end
+
+@implementation DetailsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
